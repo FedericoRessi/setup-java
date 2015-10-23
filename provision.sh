561ccde1a193448ee2d0fffa2817b09602d9bdf8
Federico Ressi <federico.ressi@intel.com>
2015-10-23 17:07:12 +0100

    Git is not required.

--- provision.sh
+++ provision.sh
@@ -6,15 +6,5 @@ source /etc/profile
 
 if apt-get --version; then
     apt-get update -y
-    apt-get install -y git realpath
-
-else
-    if dnf --version; then
-        dnf install -y git
-    elif yum --version; then
-        yum install -y git
-    else
-        echo "Unsupported system."
-        exit 1
-    fi
+    apt-get install -y realpath
 fi
